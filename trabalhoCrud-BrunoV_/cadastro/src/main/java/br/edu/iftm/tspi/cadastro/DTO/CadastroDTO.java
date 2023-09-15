797383190b6b44e98a3ddf075790cd1498833e01
Bruno Vieira <133446390+brunovcunha@users.noreply.github.com>
2023-09-15 18:05:49 -0300

    Add files via upload

---
+++ trabalhoCrud-BrunoV_/cadastro/src/main/java/br/edu/iftm/tspi/cadastro/DTO/CadastroDTO.java
@@ -0,0 +1,10 @@
+package br.edu.iftm.tspi.cadastro.DTO;
+
+import lombok.Data;
+
+@Data
+public class CadastroDTO {
+    private String name;
+    private String depart;
+    private String telefone;
+}
